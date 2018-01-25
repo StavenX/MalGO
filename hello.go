7ecc85ac2b070917309b70c91e5029a10c50799d
Endrebg <Endre.b.g@gmail.com>
2018-01-25 18:34:03 +0100

    Name added
    I added the kode with my name

--- hello.go
+++ hello.go
@@ -1,4 +1,5 @@
 package main
+﻿package main
 
 import "fmt"
 
@@ -6,4 +7,5 @@ func main() {
   fmt.PrintLn("Hello 世界, Edvin's commit")
   fmt.PrintLn("Hello Shalom, Tord is the name")
   fmt.PrintLn("Hello, my name is Adrian")
+  fmt.PrintLn("Hey, Endre here")
 }
