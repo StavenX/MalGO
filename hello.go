da7a8f0c6831303f1a4f5912e7297026f82d70c4
Unknown <edvin_myhren@hotmail.com>
2018-01-26 10:19:43 +0100

    fixed "PrintLn" to "Println"

--- hello.go
+++ hello.go
@@ -4,8 +4,8 @@ package main
 import "fmt"
 
 func main() {
-  fmt.PrintLn("Hello 世界, Edvin's commit")
-  fmt.PrintLn("Hello Shalom, Tord is the name")
-  fmt.PrintLn("Hello, my name is Adrian")
-  fmt.PrintLn("Hey, Endre here")
+  fmt.Println("Hello 世界, Edvin's commit")
+  fmt.Println("Hello Shalom, Tord is the name")
+  fmt.Println("Hello, my name is Adrian")
+  fmt.Println("Hey, Endre here")
 }
